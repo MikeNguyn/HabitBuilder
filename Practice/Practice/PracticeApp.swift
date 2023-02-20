098716f606cab3e6471017c03016d70522a815e3
matt3mukai <77793859+matt3mukai@users.noreply.github.com>
2023-02-20 13:06:38 -0800

    monday grid stuff

---
+++ Practice/Practice/PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeApp.swift
+//  Practice
+//
+//  Created by Matthew Mukai on 2/8/23.
+//
+
+import SwiftUI
+
+@main
+struct PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
