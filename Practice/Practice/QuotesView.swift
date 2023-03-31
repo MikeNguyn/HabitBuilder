ffcab25544c2c5e2c10a76b63f2448f73567bd9e
franwb <frannyb1453@gmail.com>
2023-03-31 13:33:07 -0500

    hard coded in a quote

--- Practice/Practice/QuotesView.swift
+++ Practice/Practice/QuotesView.swift
@@ -8,13 +8,22 @@
 import SwiftUI
 
 struct QuotesView: View {
+//    let myQuote: QuotesManager
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//        Text(myQuote.quote)
+//            .position(x:100,y:100)
+        Text("One day, or day one? You decide.")
+            .position(x:200,y:100).foregroundColor(.teal).font(.system(size: 20)
+                .bold())
+//            .frame(width: screenSize.width/8, height: screenSize.width/5).
     }
 }
 
 struct QuotesView_Previews: PreviewProvider {
     static var previews: some View {
+//        let myQuote: QuotesManager = QuotesManager()
+//        QuotesView(myQuote: myQuote)
         QuotesView()
     }
+
 }
