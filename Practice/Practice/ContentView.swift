cb005e0604df6dc1d31f05c6cbb1dcbd90385749
CurranM <cmiddle1@macalester.edu>
2023-04-11 16:25:08 -0500

    Added a basic notification button
    :)

--- Practice/Practice/ContentView.swift
+++ Practice/Practice/ContentView.swift
@@ -7,6 +7,7 @@
 //The main front screen/page. It displays the main grid of plants and other icons.
 
 import SwiftUI
+import UserNotifications
 let MENUICONSIZE = 60.0
 
 
@@ -20,6 +21,31 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             VStack{
+                Button("Request Permission") {
+                    UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                        if success {
+                            print("All set!")
+                        } else if let error = error {
+                            print(error.localizedDescription)
+                        }
+                    }
+                }
+                
+                Button("Schedule Notification") {
+                    let content = UNMutableNotificationContent()
+                    content.title = "Feed the cat"
+                    content.subtitle = "It looks hungry"
+                    content.sound = UNNotificationSound.default
+                    
+                    // show this notification five seconds from now
+                    let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+                    
+                    //choose a random identifier
+                    let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+                    
+                    // add our notification request
+                    UNUserNotificationCenter.current().add(request)
+                }
                 Grid(){
                     Divider()
                     GridRow {
