d6d6d7c28ad70cb98fad77d61c2096e8865dd282
matt3mukai <matt3mukai@gmail.com>
2023-04-26 15:08:41 -0500

    notifications asked on opening

--- Practice/Practice/PracticeApp.swift
+++ Practice/Practice/PracticeApp.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct PracticeApp: App {
     //    @State var habitManager = HabitManager()
     @StateObject private var store = HabitStore()
+    @State private var notificationsAllowed = false
     var body: some Scene {
         WindowGroup {
             ContentView(habitList: $store.habits) {
@@ -27,6 +28,16 @@ struct PracticeApp: App {
                         fatalError(error.localizedDescription)
                     case .success(let habits):
                         store.habits = habits
+                    }
+                }
+                if (notificationsAllowed == false){
+                    UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                        if success {
+                            print("All set!")
+                            notificationsAllowed = true
+                        } else if let error = error {
+                            print(error.localizedDescription)
+                        }
                     }
                 }
             }
